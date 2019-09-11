34b569e7bdb04c4486208bd0ef294f279abdc0c1
Craig <craigoda@gmail.com>
2019-09-11 09:31:41 -0700

    created models

--- gallery/theta_image/models.py
+++ gallery/theta_image/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Thetaimage(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=80)
+    summary = models.CharField(max_length=160)
