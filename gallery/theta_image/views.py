99ff9182a11d2291a796b86190815a239cceb1e2
Craig <craigoda@gmail.com>
2019-09-11 18:06:18 -0700

    added images

--- gallery/theta_image/views.py
+++ gallery/theta_image/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Thetaimage
 
 
 def home(request):
-    return render(request, 'theta_images/home.html')
+    images = Thetaimage.objects
+    return render(
+        request,
+        'theta_images/home.html',
+        {'images': images})
